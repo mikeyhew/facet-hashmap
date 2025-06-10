5b0831f0b86b66aaed73e6840de449da4dfbf93c
Michael Hewson <michael@michaelhewson.ca>
2025-06-10 16:48:36 -0400

    move facet_hash_map into its own module

---
+++ src/facet_hashmap.rs
@@ -0,0 +1,26 @@
+use std::hash::{BuildHasher, Hash};
+
+use facet::Facet;
+
+use crate::erased::Erased;
+use crate::erased_hashmap::{ErasedHashMap, ErasedKey, ErasedValue};
+
+pub struct FacetHashMap<K, V, S = hashbrown::DefaultHashBuilder> {
+    hash_map: ErasedHashMap<S>,
+    _marker: std::marker::PhantomData<(K, V)>,
+}
+
+impl<K, V, S> FacetHashMap<K, V, S> {
+    pub fn insert<'a>(&mut self, key: K, value: V) -> Option<V>
+    where
+        K: Facet<'a> + Hash + Eq,
+        V: Facet<'a>,
+        S: BuildHasher,
+    {
+        let erased_key = ErasedKey(Erased::new(key));
+        let erased_value = ErasedValue(Erased::new(value));
+        let old_erased_value = unsafe { self.hash_map.insert(erased_key, K::SHAPE, erased_value) };
+
+        old_erased_value.map(|old_value| unsafe { old_value.0.into_typed() })
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,5 @@
-use std::hash::{BuildHasher, Hash};
-
-use facet::Facet;
-
 mod erased;
 mod erased_hashmap;
+mod facet_hashmap;
 
-use erased::Erased;
-use erased_hashmap::{ErasedHashMap, ErasedKey, ErasedValue};
-
-pub struct FacetHashMap<K, V, S = hashbrown::DefaultHashBuilder> {
-    hash_map: ErasedHashMap<S>,
-    _marker: std::marker::PhantomData<(K, V)>,
-}
-
-impl<K, V, S> FacetHashMap<K, V, S> {
-    pub fn insert<'a>(&mut self, key: K, value: V) -> Option<V>
-    where
-        K: Facet<'a> + Hash + Eq,
-        V: Facet<'a>,
-        S: BuildHasher,
-    {
-        let erased_key = ErasedKey(Erased::new(key));
-        let erased_value = ErasedValue(Erased::new(value));
-        let old_erased_value = unsafe { self.hash_map.insert(erased_key, K::SHAPE, erased_value) };
-
-        old_erased_value.map(|old_value| unsafe { old_value.0.into_typed() })
-    }
-}
+pub use facet_hashmap::FacetHashMap;
